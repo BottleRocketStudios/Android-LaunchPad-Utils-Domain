1799b5dba37cfc03aa6ad83373143b56925b31db
Eric Emery <eric.emery@bottlerocketstudios.com>
2023-03-22 14:22:04 -0500

    ASAA-56 - Initial Commit

---
+++ launchpad-utils-domain/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id(Config.ApplyPlugins.ANDROID_LIBRARY)
+    kotlin(Config.ApplyPlugins.Kotlin.ANDROID)
+    id(Config.ApplyPlugins.MAVEN_PUBLISH)
+}
+
+android {
+    compileSdk = Config.AndroidSdkVersions.COMPILE_SDK
+    buildToolsVersion = Config.AndroidSdkVersions.BUILD_TOOLS
+    namespace = "com.bottlerocketstudios.launchpad.utils.domain"
+
+    defaultConfig {
+        minSdk = Config.AndroidSdkVersions.MIN_SDK
+        targetSdk = Config.AndroidSdkVersions.TARGET_SDK
+        aarMetadata {
+            minCompileSdk = Config.AndroidSdkVersions.MIN_COMPILE_SDK
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.github.BottleRocketStudios"
+                artifactId = "Android-LaunchPad-Utils-Domain"
+                version = "0.0.1"
+
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+//        repositories {
+//            maven {
+//                name = "myrepo"
+//                url = uri("${project.buildDir}/repo")
+//            }
+//        }
+    }
+}
+
+dependencies {
+    kotlinDependencies()
+    coroutineCoreDependency()
+    encryptedPrefsDependencies()
+    timberDependencies()
+    firebaseAnalyticsDependencies()
+}
