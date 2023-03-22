7aa1bdb289f3cb59388e2971744f37196d9b681f
Eric Emery <eric.emery@bottlerocketstudios.com>
2023-03-22 14:23:01 -0500

    ASAA-56 - Added domain files from launchpad-utils library

---
+++ launchpad-utils-domain/src/main/java/com/bottlerocketstudios/launchpad/utils/domain/analytics/UserProperty.kt
@@ -0,0 +1,10 @@
+package com.bottlerocketstudios.launchpad.utils.domain.analytics
+
+/** Used in [AnalyticsInterface.setUserProperty]. */
+interface UserProperty : BaseAnalyticsUserProperty {
+    val propertyName: String
+    val propertyValue: String
+
+    override fun createUserProperty(): UserProperty =
+        CustomUserProperty(propertyName, propertyValue)
+}
